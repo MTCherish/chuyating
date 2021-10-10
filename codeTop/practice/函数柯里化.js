c629fa46ef4321a19e47ee3b7763d65baca7e991
MTCherish <584825663@qq.com>
2021-10-10 08:30:32 +0800

    practice

---
+++ codeTop/practice/函数柯里化.js
@@ -0,0 +1,14 @@
+function sum(...rest){
+    var _args=Array.prototype.slice.call(arguments);
+    var _adder=function(){
+        _args.push(...arguments);
+        return _adder;
+    }
+    _adder.toString=function(){
+        let sum=_args.reduce((a,b)=>a+b);
+        return sum;
+    }
+    return _adder;
+}
+let sumToNumber=Number.parseInt(sum(1)(2)(3))
+console.log(sumToNumber);
---
+++ codeTop/practice/节流和防抖.js
@@ -0,0 +1,21 @@
+function thorttle(fn,delay){
+    var startTime=0;
+    return function(){
+        nowTime=new Date();
+        if(nowTime-startTime>delay){
+            fn();
+            startTime=nowTime;
+        }
+    }
+}
+
+function debounce(fn,delay){
+    let timer=null;
+    return function(...arguments){
+        let that=this;
+        if(timer) clearTimeout(timer);
+        timer=setTimeout(()=>{
+            fn.apply(that,arguments);
+        },delay)
+    }
+}
